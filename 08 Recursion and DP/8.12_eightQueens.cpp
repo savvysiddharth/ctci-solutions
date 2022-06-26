baa7bf24331a337658e16129dd6c2b27065f2f91
Siddharth Maurya <savvysiddharth@gmail.com>
2022-06-26 20:16:53 +0530

    optimized n queens

--- 08 Recursion and DP/8.12_eightQueens.cpp
+++ 08 Recursion and DP/8.12_eightQueens.cpp
@@ -6,27 +6,13 @@ using namespace std;
 
 // Time: O(n^2) <- for valid configuration
 // Space: O(n)
-bool onDiagonal(vector<int> queen, vector<vector<int>> &queens) {
-  for(auto otherQueen: queens) {
+bool onDiagonal(vector<int> &queen, vector<vector<int>> &queens) {
+  for(auto &otherQueen: queens) {
     if(otherQueen[1] == queen[1] || otherQueen[0] == queen[0]) continue;
     double slope = 1.0*(otherQueen[0] - queen[0])/(otherQueen[1] - queen[1]);
     if(slope == 1.0 || slope == -1.0) return true;
   }
   return false;
-}
-
-bool isValid(vector<vector<int>> &queens) {
-  unordered_set<int> rows, cols;
-  for(auto queen : queens) { // checking for row/cols
-    if(rows.find(queen[0]) != rows.end() || cols.find(queen[1]) != cols.end())
-      return false;
-    rows.insert(queen[0]);
-    cols.insert(queen[1]);
-  }
-  for(auto queen: queens) { // checking for diagonal
-    if(onDiagonal(queen, queens)) return false;
-  }
-  return true;
 }
 
 void printQueens(vector<vector<int>> &matrix) {
@@ -35,19 +21,23 @@ void printQueens(vector<vector<int>> &matrix) {
   } cout << endl;
 }
 
-void nQueens(int row, vector<vector<int>> queens, int &n, int &count, vector<vector<vector<int>>> &result) {
+void nQueens(int row, vector<vector<int>> queens, int &n, int &count, vector<vector<vector<int>>> &result, unordered_set<int> cols) {
   if(row == n) {
-    if(isValid(queens)) {
-      result.push_back(queens);
-      count++;
-      return;
-    } else return;
+    result.push_back(queens);
+    count++;
+    return;
   }
   for(int i=0; i<n; i++) {
-    vector<vector<int>> newQueens = queens;
-    queens.push_back(vector<int>{row, i});
-    if(isValid(queens)) nQueens(row+1, queens, n, count, result);
-    queens.pop_back();
+    if(cols.find(i) == cols.end()) {
+      vector<int> nextQueen {row, i};
+      if(!onDiagonal(nextQueen, queens)) {
+        cols.insert(i);
+        queens.push_back(nextQueen);
+        nQueens(row+1, queens, n, count, result, cols);
+        queens.pop_back();
+        cols.erase(i);
+      }
+    }
   }
 }
 
@@ -63,10 +53,8 @@ int nQueens(int n) {
   vector<vector<int>> queens{};
   vector<vector<vector<int>>> result;
   int count = 0;
-  nQueens(0, queens, n, count, result);
-  // for(auto config : result) {
-  //   printQueens(config);
-  // }
+  unordered_set<int> cols;
+  nQueens(0, queens, n, count, result, cols);
   vector<vector<string>> answer {};
   for(int i=0; i<count; i++) {
     vector<string> currentBoard {};
@@ -80,17 +68,17 @@ int nQueens(int n) {
     }
     answer.push_back(currentBoard);
   }
-  // printAnswers(answer);
   return count;
 }
 
 // queens[i][0] <= x-cord of i'th queen
 // queens[i][1] <= y-cord of i'th queen
 int main() {
-  int n = 4;
+  int n = 8;
   cout << nQueens(n) << endl;
 
+  // Example queens vector
   // vector<vector<int>> queens {{0,5},{1,3},{2,6},{3,0},{4,7},{5,1},{6,4},{7,2}};
-  // if(isValid(queens)) cout << "is valid.." << endl;
-  // else cout << "NOT valid.." << endl;
 }
+
+// Tested on: https://leetcode.com/problems/n-queens/
