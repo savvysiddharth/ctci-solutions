fb3dd407e9c8ffe100cfa4f4df1eee57a40d8c2f
Siddharth Maurya <savvysiddharth@gmail.com>
2022-06-30 12:54:28 +0530

    added comments for 10.3

--- 10 Sorting and Searching/10.3_searchRotatedArray.cpp
+++ 10 Sorting and Searching/10.3_searchRotatedArray.cpp
@@ -8,14 +8,14 @@ int search(vector<int> &arr, int target) {
     int mid = (low + high)/2;
     if(arr[mid] == target) return mid;
     if(target > arr[mid]) {
-      if(arr[mid] > arr[high]) low = mid+1;
-      else if(target > arr[high]) high = mid-1; // go left
+      if(arr[mid] > arr[high]) low = mid+1; // if right is non-sorted, it must be there
+      else if(target > arr[high]) high = mid-1; // go left, if right sorted and target exceeds highest right
       else low = mid+1; // go right
     }
     if(target < arr[mid]) {
-      if(arr[mid] < arr[low]) high = mid-1;
-      else if(target < arr[low]) low = mid+1;
-      else high = mid-1;
+      if(arr[mid] < arr[low]) high = mid-1; // if left is non-sorted, it must be there
+      else if(target < arr[low]) low = mid+1; // go right, if left sorted and target is lower than lowest left
+      else high = mid-1; // go left
     }
   }
   return -1;
